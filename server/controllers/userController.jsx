b22808acea4e75b7993df3ff38ec52ce9b7d90ed
Yiannis Kalogiannis <kalojohn22@gmail.com>
2024-12-17 23:00:56 +0200

    register controller

--- server/controllers/userController.jsx
+++ server/controllers/userController.jsx
@@ -62,3 +62,72 @@ if (!email || !password) {
   res.status(500).send(`backend: User not found, please try again later`);
 }
 };
+
+
+
+
+/ _________________create new user and save in DB for register_______________
+
+const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/;
+
+let addNewUser = async (req, res) => {
+  const { firstName, lastName, email, password, } = req.body;
+  try {
+    if (!firstName || !lastName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required!!" });
+    }
+    if (!passwordRegex.test(password)) {
+      return res.status(400).json({
+        message:
+          "Password must be at least 8 characters and contain both letters and numbers!",
+      });
+    }
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+      console.log({ message: "Email already exists" });
+      return res.status(400).json({
+        message: `Email already exists!, login or please register with different credentials`,
+      });
+    }
+
+
+     //  Handle the uploaded image (optional)
+     let profileImage = null;
+     if (req.file) {
+       profileImage = req.file.filename; // The filename assigned by multer
+      }
+
+    const hashPassword = await bcrypt.hash(password, saltRounds);
+    const newUser = {
+      firstName,
+      lastName,
+      email,
+      password: hashPassword,
+      image: profileImage,
+    };
+    const createdUser = await User.create(newUser);
+
+    // token implementation
+    let payload = {
+      firstName: createdUser.firstName,
+      lastName: createdUser.lastName,
+      image: createdUser.image,
+      id: createdUser._id,
+    };
+
+    let token = jwt.sign(payload, process.env.SECRET_KEY);
+
+    // console.log(token);
+
+    console.log({ message: "User created successfully" });
+
+    return res.status(201).json({
+      message: "User created successfully",
+      user: createdUser,
+      token,
+    });
+  } catch (error) {
+    console.log(`Error: ${error}`);
+    res.status(500).json({ message: `backend: Failed to create a new user` });
+  }
+};
