fde289a35f78397845a166dafa94c28119416712
Yiannis Kalogiannis <kalojohn22@gmail.com>
2024-12-17 22:44:29 +0200

    user schema created

---
+++ server/schemas/usersSchema.jsx
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+},
+firstName: {
+    type: String,
+    required: true,
+    trim: true,
+},
+lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+    profilePicture: {
+        type: String,
+        default: '',
+    },
+    timestamps: true
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
